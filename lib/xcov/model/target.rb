5d14ad8f33fc58f5e83243f8dd217a4f0766a663
Farees <120588443+fareessyed-cred@users.noreply.github.com>
2024-02-14 13:58:34 +0530

    Update target.rb

--- lib/xcov/model/target.rb
+++ lib/xcov/model/target.rb
@@ -10,9 +10,9 @@ module Xcov
     def initialize(name, files)
       @name = CGI::escapeHTML(name)
       @files = files
-      totalCoveredLines = files.reduce(0) { |acc, file| acc + file.coveredLines }
-      totalExecutableLines = files.reduce(0) { |acc, file| acc + file.executableLines }
-      @coverage = files.count == 0 || totalExecutableLines == 0 ? 0.0 : totalCoveredLines.to_f / totalExecutableLines.to_f
+      @total_covered_lines = files.reduce(0) { |acc, file| acc + file.coveredLines }
+      @total_executable_lines = files.reduce(0) { |acc, file| acc + file.executableLines }
+      @coverage = files.count == 0 || @total_executable_lines == 0 ? 0.0 : @total_covered_lines.to_f / @total_executable_lines.to_f
       @displayable_coverage = self.create_displayable_coverage
       @coverage_color = self.create_coverage_color
       @id = Target.create_id(name)
